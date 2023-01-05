464809fb43fe189c36be33e4329327ae15e9d560
Lanlyhs <ziqiang.chen@outlook.com>
2023-01-05 20:41:05 +0900

    Update add Median of Two Sorted Arrays

---
+++ test/004-median-of-two-sorted-arrays.test.js
@@ -0,0 +1,12 @@
+const { findMedianSortedArrays } = require("../src/004-median-of-two-sorted-arrays");
+
+describe("Median of Two Sorted Arrays", () => {
+    it("2.00000", () => {
+        expect(findMedianSortedArrays([1, 3], [2])).toEqual(2.00000);
+    });
+
+    it("2.50000", () => {
+        expect(findMedianSortedArrays([1, 2], [3, 4])).toEqual(2.50000);
+    });
+
+});
