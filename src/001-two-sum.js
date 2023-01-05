0c3dba620b0da2fba4949efc4ac6bc36565267fb
Lanlyhs <ziqiang.chen@outlook.com>
2023-01-06 00:16:34 +0900

    Refact test for mulit function

--- src/001-two-sum.js
+++ src/001-two-sum.js
@@ -1,18 +1,41 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-module.exports = function (nums, target) {
-	if (nums.length < 2) {
-		throw new Error("nums length can't smaller than 2");
-	}
-	for (let i = 0; i < nums.length; i++) {
-		for (let j = i + 1; j < nums.length; j++) {
-			if (nums[i] + nums[j] === target) {
-				return [i, j];
+
+module.exports = [
+	/**
+	 * @param {number[]} nums
+	 * @param {number} target
+	 * @return {number[]}
+	 */
+	function twoSum(nums, target) {
+		if (nums.length < 2) {
+			throw new Error("nums length can't smaller than 2");
+		}
+		for (let i = 0; i < nums.length; i++) {
+			for (let j = i + 1; j < nums.length; j++) {
+				if (nums[i] + nums[j] === target) {
+					return [i, j];
+				}
 			}
 		}
-	}
-	throw new Error("can't find result");
-};
+		throw new Error("can't find result");
+	},
+	/**
+	 * @param {number[]} nums
+	 * @param {number} target
+	 * @return {number[]}
+	 */
+	function twoSum1(nums, target) {
+		if (nums.length < 2) {
+			throw new Error("nums length can't smaller than 2");
+		}
+		// or use Map
+		const numIndexObj = {};
+		for (let i = 0; i < nums.length; i++) {
+			const remainValue = target - nums[i];
+			if (numIndexObj[remainValue] !== undefined) {
+				return [numIndexObj[remainValue], i];
+			}
+			numIndexObj[nums[i]] = i;
+		}
+		throw new Error("can't find result");
+	},
+];
--- test/001-two-sum.test.js
+++ test/001-two-sum.test.js
@@ -1,15 +1,19 @@
-const twoSum = require("../src/001-two-sum-1");
+const fns = require("../src/001-two-sum");
 
 describe("Two Sum", () => {
-	it("expect [0,1]", () => {
-		expect(twoSum([1, 2], 3)).toEqual(expect.arrayContaining([0, 1]));
-	});
+	fns.forEach(fn => {
+		describe(fn.name, () => {
+			it("expect [0,1]", () => {
+				expect(fn([1, 2], 3)).toEqual(expect.arrayContaining([0, 1]));
+			});
 
-	it("expect [0,1]", () => {
-		expect(twoSum([2, 7, 11, 15], 9)).toEqual(expect.arrayContaining([0, 1]));
-	});
+			it("expect [0,1]", () => {
+				expect(fn([2, 7, 11, 15], 9)).toEqual(expect.arrayContaining([0, 1]));
+			});
 
-	it("expect [1,2]", () => {
-		expect(twoSum([3, 2, 4], 6)).toEqual(expect.arrayContaining([1, 2]));
-	});
+			it("expect [1,2]", () => {
+				expect(fn([3, 2, 4], 6)).toEqual(expect.arrayContaining([1, 2]));
+			});
+		});
+	})
 });
