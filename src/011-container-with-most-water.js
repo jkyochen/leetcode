d17cbedc94721982e954a2526c08ead4cb3b7da7
Lanlyhs <ziqiang.chen@outlook.com>
2023-01-06 21:23:48 +0900

    Update add new answer for Container With Most Water

--- src/011-container-with-most-water.js
+++ src/011-container-with-most-water.js
@@ -1,13 +1,29 @@
-/**
- * @param {number[]} height
- * @return {number}
- */
 module.exports = [
+	/**
+	 * @param {number[]} height
+	 * @return {number}
+	 */
 	function maxArea(height) {
 		let area = 0;
 		for (let i = 0; i < height.length; i++) {
 			for (let j = i + 1; j < height.length; j++) {
 				area = Math.max(area, (j - i) * Math.min(height[i], height[j]));
+			}
+		}
+		return area;
+	},
+	/**
+	 * @param {number[]} height
+	 * @return {number}
+	 */
+	function maxArea1(height) {
+		let area = 0;
+		for (let i = 0, j = height.length - 1; i < j; ) {
+			area = Math.max(area, (j - i) * Math.min(height[i], height[j]));
+			if (height[i] <= height[j]) {
+				i++;
+			} else {
+				j--;
 			}
 		}
 		return area;
