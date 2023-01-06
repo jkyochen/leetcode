7393b5bd5d3ea09f9d73915e82eb35c8d642429a
Lanlyhs <ziqiang.chen@outlook.com>
2023-01-06 14:40:25 +0900

    Update Palindrome Number new answer

--- src/009-palindrome-number.js
+++ src/009-palindrome-number.js
@@ -6,4 +6,13 @@ module.exports = [
     function isPalindrome(x) {
         return String(x) === String(x).split("").reverse().join("");
     },
+    function isPalindrome1(x) {
+        x = String(x);
+        for (let i = 0, j = x.length - 1; i < j; i++, j--) {
+            if (x[i] !== x[j]) {
+                return false;
+            }
+        }
+        return true;
+    },
 ];
