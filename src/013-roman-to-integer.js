047751659487df6791977477d0732fc2851da6b3
Lanlyhs <ziqiang.chen@outlook.com>
2023-01-08 23:13:28 +0900

    Update prepare for romanToInt mulit answer

--- src/013-roman-to-integer.js
+++ src/013-roman-to-integer.js
@@ -1,16 +1,18 @@
 const { specialRomans } = require("./012-integer-to-roman");
 
-/**
- * @param {string} s
- * @return {number}
- */
-module.exports.romanToInt = function (s) {
-	return Object.entries(specialRomans)
-		.map(([k, v]) => ({ value: k, romanChar: v }))
-		.sort((a, b) => b.romanChar.length - a.romanChar.length)
-		.reduce((acc, cur) => {
-			let remainRomans = s.split(cur.romanChar);
-			s = remainRomans.join("");
-			return (acc += cur.value * (remainRomans.length - 1));
-		}, 0);
-};
+module.exports = [
+	/**
+	 * @param {string} s
+	 * @return {number}
+	 */
+	function romanToInt(s) {
+		return Object.entries(specialRomans)
+			.map(([k, v]) => ({ value: k, romanChar: v }))
+			.sort((a, b) => b.romanChar.length - a.romanChar.length)
+			.reduce((acc, cur) => {
+				let remainRomans = s.split(cur.romanChar);
+				s = remainRomans.join("");
+				return (acc += cur.value * (remainRomans.length - 1));
+			}, 0);
+	},
+];
--- test/013-roman-to-integer.test.js
+++ test/013-roman-to-integer.test.js
@@ -1,23 +1,25 @@
-const { romanToInt } = require(require(".")(__filename).sourcePath);
+const fns = require(require(".")(__filename).sourcePath);
 
-describe("Roman to Integer", () => {
-	it("III", () => {
-		expect(romanToInt("III")).toEqual(3);
-	});
+fns.forEach((fn) => {
+	describe(fn.name, () => {
+		it("III", () => {
+			expect(fn("III")).toEqual(3);
+		});
 
-	it("IV", () => {
-		expect(romanToInt("IV")).toEqual(4);
-	});
+		it("IV", () => {
+			expect(fn("IV")).toEqual(4);
+		});
 
-	it("IX", () => {
-		expect(romanToInt("IX")).toEqual(9);
-	});
+		it("IX", () => {
+			expect(fn("IX")).toEqual(9);
+		});
 
-	it("LVIII", () => {
-		expect(romanToInt("LVIII")).toEqual(58);
-	});
+		it("LVIII", () => {
+			expect(fn("LVIII")).toEqual(58);
+		});
 
-	it("MCMXCIV", () => {
-		expect(romanToInt("MCMXCIV")).toEqual(1994);
+		it("MCMXCIV", () => {
+			expect(fn("MCMXCIV")).toEqual(1994);
+		});
 	});
 });
