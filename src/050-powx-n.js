c7901b0d00f89ade01eb1f294e29386054f53c80
Lanlyhs <ziqiang.chen@outlook.com>
2023-01-17 21:52:21 +0900

    Update add new answer for pow

--- src/050-powx-n.js
+++ src/050-powx-n.js
@@ -26,4 +26,26 @@ module.exports = [
 			return v ** 2 * (isOdd ? x : 1);
 		}
 	},
+	/**
+	 * @param {number} x
+	 * @param {number} n
+	 * @return {number}
+	 */
+	function myPow2(x, n) {
+		return n >= 0 ? innerPow(x, n) : 1 / innerPow(x, -n);
+		function innerPow(x, n) {
+			let v = 1;
+			let contribute = x;
+			// (x**2)**0,(x**2)**1,(x**2)**2,(x**2)**3,(x**2)**4,(x**2)**k
+			// x**1,x**2,x**4,x**8,x**16,x**32,x**64
+			while (n > 0) {
+				if (n % 2 === 1) {
+					v *= contribute;
+				}
+				contribute *= contribute;
+				n = Math.floor(n / 2);
+			}
+			return v;
+		}
+	},
 ];
