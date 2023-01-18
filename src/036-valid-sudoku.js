5e0ea9f6bfbf871a9792445bc5f003be1ad94825
Lanlyhs <ziqiang.chen@outlook.com>
2023-01-18 22:11:05 +0900

    Update add 36. Valid Sudoku

---
+++ src/036-valid-sudoku.js
@@ -0,0 +1,51 @@
+module.exports = [
+	/**
+	 * @param {character[][]} board
+	 * @return {boolean}
+	 */
+	function isValidSudoku(board) {
+		for (let i = 0; i < board.length; i++) {
+			const row = board[i].filter((r) => r !== ".");
+			if (row.length !== [...new Set(row)].length) {
+				return false;
+			}
+		}
+
+		for (let x = 0; x < 9; x++) {
+			let column = [];
+			for (let y = 0; y < 9; y++) {
+				column.push(board[y][x]);
+			}
+			column = column.filter((r) => r !== ".");
+			if (column.length !== [...new Set(column)].length) {
+				return false;
+			}
+		}
+
+		let grids = [];
+		for (let i = 0; i < 9; i++) {
+			let x = Math.floor(i / 3);
+			if (!grids[x]) {
+				grids[x] = [];
+			}
+			for (let j = 0; j < 9; j++) {
+				let y = Math.floor(j / 3);
+				if (!grids[x][y]) {
+					grids[x][y] = [];
+				}
+				grids[x][y].push(board[i][j]);
+			}
+		}
+
+		for (let i = 0; i < grids.length; i++) {
+			for (let j = 0; j < grids[i].length; j++) {
+				let grid = grids[i][j].filter((r) => r !== ".");
+				if (grid.length !== [...new Set(grid)].length) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	},
+];
