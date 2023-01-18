41f2b5c9cbee573174d2a867eaddfc2ab4dcbfdd
Lanlyhs <ziqiang.chen@outlook.com>
2023-01-18 21:06:23 +0900

    Update add new answer for 034

--- src/034-find-first-and-last-position-of-element-in-sorted-array.js
+++ src/034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -43,4 +43,36 @@ module.exports = [
 		}
 		return [-1, -1];
 	},
+	/**
+	 * @param {number[]} nums
+	 * @param {number} target
+	 * @return {number[]}
+	 */
+	function searchRange2(nums, target) {
+		let startIndex = binarySearch(nums, target, true);
+		let endIndex = binarySearch(nums, target) - 1;
+		if (
+			startIndex <= endIndex &&
+			endIndex <= nums.length - 1 &&
+			nums[startIndex] === target &&
+			nums[endIndex] === target
+		) {
+			return [startIndex, endIndex];
+		}
+		return [-1, -1];
+
+		function binarySearch(nums, target, isNeedEqual) {
+			let matchIndex = nums.length;
+			for (let start = 0, end = nums.length - 1; start <= end; ) {
+				let i = Math.floor((start + end) / 2);
+				if (nums[i] > target || (isNeedEqual && nums[i] === target)) {
+					end = i - 1;
+					matchIndex = i;
+				} else {
+					start = i + 1;
+				}
+			}
+			return matchIndex;
+		}
+	},
 ];
