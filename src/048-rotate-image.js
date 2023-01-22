0f82d94561b57e5477a8c53ca365bb48274e88c2
Lanlyhs <ziqiang.chen@outlook.com>
2023-01-22 11:12:05 +0900

    Update add new answer for 48. Rotate Image

--- src/048-rotate-image.js
+++ src/048-rotate-image.js
@@ -16,4 +16,22 @@ module.exports = [
 			matrix[i] = newMatrix[i];
 		}
 	},
+	/**
+	 * @param {number[][]} matrix
+	 * @return {void} Do not return anything, modify matrix in-place instead.
+	 */
+	function rotate1(matrix) {
+		// n**2/4=(n/2)×(n/2)
+		// (n**2−1)/4=((n−1)/2)×((n+1)/2)
+		for (let i = 0; i < Math.floor(matrix.length / 2); i++) {
+			for (let j = 0; j < Math.floor((matrix.length + 1) / 2); j++) {
+				let temp = matrix[j][matrix.length - 1 - i];
+				matrix[j][matrix.length - 1 - i] = matrix[i][j];
+				matrix[i][j] = matrix[matrix.length - 1 - j][i];
+				matrix[matrix.length - 1 - j][i] =
+					matrix[matrix.length - 1 - i][matrix.length - 1 - j];
+				matrix[matrix.length - 1 - i][matrix.length - 1 - j] = temp;
+			}
+		}
+	},
 ];
