532911e0207e7707c38100e664ffe5c4c692c752
Lanlyhs <ziqiang.chen@outlook.com>
2023-01-25 00:23:32 +0900

    Update change 002 to export array

--- src/002-add-two-numbers.js
+++ src/002-add-two-numbers.js
@@ -1,41 +1,39 @@
 const { ListNode } = require("../util");
 
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
-function addTwoNumbers(l1, l2) {
-	let isExistCarry = false;
-	let curNode1 = l1;
-	let curNode2 = l2;
-	while (curNode1 && curNode2) {
-		let sum = curNode1.val + curNode2.val;
-		if (isExistCarry) {
-			sum++;
+module.exports = [
+	/**
+	 * @param {ListNode} l1
+	 * @param {ListNode} l2
+	 * @return {ListNode}
+	 */
+	function addTwoNumbers(l1, l2) {
+		let isExistCarry = false;
+		let curNode1 = l1;
+		let curNode2 = l2;
+		while (curNode1 && curNode2) {
+			let sum = curNode1.val + curNode2.val;
+			if (isExistCarry) {
+				sum++;
+			}
+			if (sum < 10) {
+				isExistCarry = false;
+				curNode2.val = sum;
+			} else {
+				isExistCarry = true;
+				curNode2.val = sum % 10;
+			}
+			if (curNode1.next && !curNode2.next) {
+				curNode2.next = new ListNode(0);
+			}
+			if (!curNode1.next && curNode2.next) {
+				curNode1.next = new ListNode(0);
+			}
+			if (!(curNode1.next || curNode2.next) && isExistCarry) {
+				curNode2.next = new ListNode(1);
+			}
+			curNode1 = curNode1.next;
+			curNode2 = curNode2.next;
 		}
-		if (sum < 10) {
-			isExistCarry = false;
-			curNode2.val = sum;
-		} else {
-			isExistCarry = true;
-			curNode2.val = sum % 10;
-		}
-		if (curNode1.next && !curNode2.next) {
-			curNode2.next = new ListNode(0);
-		}
-		if (!curNode1.next && curNode2.next) {
-			curNode1.next = new ListNode(0);
-		}
-		if (!(curNode1.next || curNode2.next) && isExistCarry) {
-			curNode2.next = new ListNode(1);
-		}
-		curNode1 = curNode1.next;
-		curNode2 = curNode2.next;
-	}
-	return l2;
-}
-
-module.exports = {
-	addTwoNumbers,
-};
+		return l2;
+	},
+];
