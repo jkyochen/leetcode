9cf4d26c93e49bf4880486de612f92fbe8654c79
Lanlyhs <ziqiang.chen@outlook.com>
2023-01-25 16:17:56 +0900

    Add 002 init new head

--- src/002-add-two-numbers.js
+++ src/002-add-two-numbers.js
@@ -33,4 +33,23 @@ module.exports = [
 		}
 		return l2;
 	},
+	/**
+	 * @param {ListNode} l1
+	 * @param {ListNode} l2
+	 * @return {ListNode}
+	 */
+	function addTwoNumbers1(l1, l2) {
+		let head = new ListNode(0);
+		let curNode = head;
+		let carry = 0;
+		while (l1 || l2 || carry) {
+			let sum = (l1?.val || 0) + (l2?.val || 0) + carry;
+			carry = sum < 10 ? 0 : 1;
+			curNode.next = new ListNode(sum % 10);
+			curNode = curNode.next;
+			l1 = l1?.next;
+			l2 = l2?.next;
+		}
+		return head.next;
+	},
 ];
