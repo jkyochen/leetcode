adce720f490dcbe70200e22bffdf96815fe2e249
Lanlyhs <ziqiang.chen@outlook.com>
2023-01-26 11:35:30 +0900

    Update remove 003 unnecessary array

--- src/003-longest-substring-without-repeating-characters.js
+++ src/003-longest-substring-without-repeating-characters.js
@@ -4,15 +4,14 @@ module.exports = [
 	 * @return {number}
 	 */
 	function lengthOfLongestSubstring(s) {
-		let strs = Array.from(s);
 		let maxSize = 0;
-		for (let left = 0, right = 0; right < strs.length; right++) {
-			let maxLongs = strs.slice(left, right + 1);
+		for (let left = 0, right = 0; right < s.length; right++) {
+			let maxLongs = s.slice(left, right + 1);
 			if ([...new Set(maxLongs)].length === maxLongs.length) {
 				maxSize = Math.max(maxSize, maxLongs.length);
 				continue;
 			}
-			left = strs.slice(0, right).lastIndexOf(strs[right]) + 1;
+			left = s.slice(0, right).lastIndexOf(s[right]) + 1;
 		}
 		return maxSize;
 	},
