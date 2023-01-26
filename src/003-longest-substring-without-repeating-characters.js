082abaac961b84cd396e8d0346ba7ffb02ffd2fd
Lanlyhs <ziqiang.chen@outlook.com>
2023-01-26 15:47:28 +0900

    Update 003 by set or map to speed sliding window

--- src/003-longest-substring-without-repeating-characters.js
+++ src/003-longest-substring-without-repeating-characters.js
@@ -15,4 +15,41 @@ module.exports = [
 		}
 		return maxSize;
 	},
+	/**
+	 * @param {string} s
+	 * @return {number}
+	 */
+	function lengthOfLongestSubstring1(s) {
+		let uniques = new Set();
+		let maxSize = 0;
+		let right = 0;
+		for (let left = 0; left < s.length; left++) {
+			if (left !== 0) {
+				uniques.delete(s[left - 1]);
+			}
+			for (; right < s.length && !uniques.has(s[right]); right++) {
+				uniques.add(s[right]);
+			}
+			maxSize = Math.max(maxSize, uniques.size);
+		}
+		return maxSize;
+	},
+	/**
+	 * @param {string} s
+	 * @return {number}
+	 */
+	function lengthOfLongestSubstring2(s) {
+		let charIndexPair = new Map();
+		let maxSize = 0;
+		for (let left = 0, right = 0; right < s.length; right++) {
+			let c = s[right];
+			if (charIndexPair.has(c)) {
+				left = Math.max(left, charIndexPair.get(c) + 1);
+				charIndexPair.delete(c);
+			}
+			maxSize = Math.max(maxSize, right - left + 1);
+			charIndexPair.set(c, right);
+		}
+		return maxSize;
+	},
 ];
