581996733204431131b92631d1c10fab05cc9be2
Lanlyhs <ziqiang.chen@outlook.com>
2023-01-27 22:34:21 +0900

    Add 026 new answer

--- src/026-remove-duplicates-from-sorted-array.js
+++ src/026-remove-duplicates-from-sorted-array.js
@@ -22,4 +22,32 @@ module.exports = [
 		}
 		return nums.length;
 	},
+	/**
+	 * @param {number[]} nums
+	 * @return {number}
+	 */
+	function removeDuplicates1(nums) {
+		if (nums.length <= 1) {
+			return nums.length;
+		}
+		let slow = 1;
+		for (let fast = 1; fast < nums.length; fast++) {
+			if (nums[fast] !== nums[fast - 1]) {
+				nums[slow] = nums[fast];
+				slow++;
+			}
+		}
+		return slow;
+	},
+	/**
+	 * @param {number[]} nums
+	 * @return {number}
+	 */
+	function removeDuplicates2(nums) {
+		let newNums = [...new Set(nums)];
+		for (let i = 0; i < newNums.length; i++) {
+			nums[i] = newNums[i];
+		}
+		return (nums.length = newNums.length);
+	},
 ];
