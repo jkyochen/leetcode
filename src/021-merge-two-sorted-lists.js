36d97cc80e3ef275c5497acd7443a68734a39509
Lanlyhs <ziqiang.chen@outlook.com>
2023-01-27 21:55:56 +0900

    Update 021 new answer

--- src/021-merge-two-sorted-lists.js
+++ src/021-merge-two-sorted-lists.js
@@ -7,6 +7,58 @@ module.exports = [
 	 * @return {ListNode}
 	 */
 	function mergeTwoLists(list1, list2) {
+		let head = new ListNode(0);
+		let curNode = head;
+		merge();
+		return head.next;
+		function merge() {
+			if (!(list1 || list2)) {
+				return;
+			}
+			if (!list1) {
+				curNode.next = list2;
+				return;
+			}
+			if (!list2) {
+				curNode.next = list1;
+				return;
+			}
+			if (list1.val <= list2.val) {
+				curNode.next = new ListNode(list1.val);
+				list1 = list1.next;
+			} else {
+				curNode.next = new ListNode(list2.val);
+				list2 = list2.next;
+			}
+			curNode = curNode.next;
+			merge(list1, list2);
+		}
+	},
+	/**
+	 * @param {ListNode} list1
+	 * @param {ListNode} list2
+	 * @return {ListNode}
+	 */
+	function mergeTwoLists1(list1, list2) {
+		if (!list1) {
+			return list2;
+		}
+		if (!list2) {
+			return list1;
+		}
+		if (list1.val <= list2.val) {
+			list1.next = mergeTwoLists1(list1.next, list2);
+			return list1;
+		}
+		list2.next = mergeTwoLists1(list1, list2.next);
+		return list2;
+	},
+	/**
+	 * @param {ListNode} list1
+	 * @param {ListNode} list2
+	 * @return {ListNode}
+	 */
+	function mergeTwoLists2(list1, list2) {
 		let head = null;
 		let lasEle = null;
 		while (list1 !== null || list2 !== null) {
