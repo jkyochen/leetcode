40848cb00e49aa678b3110cb86ee285903e4b5cf
Lanlyhs <ziqiang.chen@outlook.com>
2023-01-29 13:46:26 +0900

    Add 007 new answer

--- src/007-reverse-integer.js
+++ src/007-reverse-integer.js
@@ -16,4 +16,20 @@ module.exports = [
 		}
 		return reverseNum;
 	},
+	/**
+	 * @param {number} x
+	 * @return {number}
+	 */
+	function reverse1(x) {
+		let rev = 0;
+		while (x !== 0) {
+			rev = rev * 10 + (x % 10);
+			if (rev < Math.pow(-2, 31) || rev > Math.pow(2, 31) - 1) {
+				return 0;
+			}
+			// x = x <= 0 ? Math.ceil(x / 10) : Math.floor(x / 10);
+			x = ~~(x / 10);
+		}
+		return rev;
+	},
 ];
