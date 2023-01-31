454da5f80267658cb75dbfd4e85660a898891205
Lanlyhs <ziqiang.chen@outlook.com>
2023-01-31 22:14:12 +0900

    Update skip same element

--- src/015-3sum.js
+++ src/015-3sum.js
@@ -7,12 +7,17 @@ module.exports = [
 		let triplets = [];
 		nums = nums.sort((a, b) => a - b);
 		for (let i = 0; i < nums.length; i++) {
+			if (i !== 0 && nums[i] === nums[i - 1]) {
+				continue;
+			}
 			for (let j = i + 1; j < nums.length; j++) {
-				for (
-					let k = j + 1;
-					k < nums.length && (k === j + 1 || nums[k] !== nums[k - 1]);
-					k++
-				) {
+				if (j !== i + 1 && nums[j] === nums[j - 1]) {
+					continue;
+				}
+				for (let k = j + 1; k < nums.length; k++) {
+					if (k !== j + 1 && nums[k] === nums[k - 1]) {
+						continue;
+					}
 					let triplet = [nums[i], nums[j], nums[k]].sort((a, b) => a - b);
 					if (
 						triplet[0] + triplet[1] + triplet[2] === 0 &&
