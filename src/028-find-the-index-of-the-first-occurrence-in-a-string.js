405ce8b846572cca044a29b633eee2067a9b2e0d
Lanlyhs <ziqiang.chen@outlook.com>
2023-02-02 15:43:53 +0900

    Add 028 new answer

--- src/028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ src/028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -7,4 +7,29 @@ module.exports = [
 	function strStr(haystack, needle) {
 		return haystack.indexOf(needle);
 	},
+	/**
+	 * @param {string} haystack
+	 * @param {string} needle
+	 * @return {number}
+	 */
+	function strStr1(haystack, needle) {
+		for (let i = 0; i < haystack.length; i++) {
+			let j = 0;
+			for (; j < needle.length; j++) {
+				if (haystack[i + j] !== needle[j]) {
+					break;
+				}
+			}
+			if (j === needle.length) {
+				return i;
+			}
+		}
+		return -1;
+	},
+	/**
+	 * @param {string} haystack
+	 * @param {string} needle
+	 * @return {number}
+	 */
+	// TODO KMP
 ];
