7275bf0375b294c749fdaaa56d0deecd5857bc1a
Lanlyhs <ziqiang.chen@outlook.com>
2023-02-07 18:58:44 +0900

    Add 040 use freq to dfs

--- src/040-combination-sum-ii.js
+++ src/040-combination-sum-ii.js
@@ -64,4 +64,92 @@ module.exports = [
 			}
 		}
 	},
+	/**
+	 * @param {number[]} candidates
+	 * @param {number} target
+	 * @return {number[][]}
+	 */
+	function combinationSum22(candidates, target) {
+		candidates.sort((a, b) => a - b);
+		let freqMap = candidates.reduce((acc, cur) => {
+			if (!acc.has(cur)) {
+				acc.set(cur, 1);
+				return acc;
+			}
+			acc.set(cur, acc.get(cur) + 1);
+			return acc;
+		}, new Map());
+		let freq = [...freqMap.entries()];
+
+		let result = [];
+		let combines = [];
+		dfs(0, 0);
+		return result;
+		function dfs(sum, idx) {
+			if (sum > target) {
+				return;
+			}
+			if (sum === target) {
+				result.push([...combines]);
+				return;
+			}
+			for (let i = idx; i < freq.length; i++) {
+				let ele = freq[i];
+				let times = Math.min(Math.floor((target - sum) / ele[0]), ele[1]);
+				for (let j = 1; j <= times; j++) {
+					combines.push(ele[0]);
+					dfs(sum + j * ele[0], i + 1);
+				}
+				// clear
+				for (let j = 0; j < times; j++) {
+					combines.pop();
+				}
+			}
+		}
+	},
+	/**
+	 * @param {number[]} candidates
+	 * @param {number} target
+	 * @return {number[][]}
+	 */
+	function combinationSum23(candidates, target) {
+		candidates.sort((a, b) => a - b);
+		let freqMap = candidates.reduce((acc, cur) => {
+			if (!acc.has(cur)) {
+				acc.set(cur, 1);
+				return acc;
+			}
+			acc.set(cur, acc.get(cur) + 1);
+			return acc;
+		}, new Map());
+		let freq = [...freqMap.entries()];
+
+		let result = [];
+		let combines = [];
+		dfs(0, 0);
+		return result;
+		function dfs(sum, idx) {
+			if (sum === target) {
+				result.push([...combines]);
+				return;
+			}
+			if (sum > target || idx === freq.length) {
+				return;
+			}
+			dfs(sum, idx + 1);
+
+			let times = Math.min(
+				Math.floor((target - sum) / freq[idx][0]),
+				freq[idx][1],
+			);
+			for (let i = 1; i <= times; i++) {
+				combines.push(freq[idx][0]);
+				dfs(sum + i * freq[idx][0], idx + 1);
+			}
+			// clear
+			for (let j = 0; j < times; j++) {
+				combines.pop();
+			}
+		}
+	},
 ];
--- test/040-combination-sum-ii.test.js
+++ test/040-combination-sum-ii.test.js
@@ -3,7 +3,7 @@ const fns = require(require(".")(__filename).sourcePath);
 fns.forEach((fn) => {
 	describe(fn.name, () => {
 		it("[10,1,2,7,6,1,5]", () => {
-			expect(fn([10, 1, 2, 7, 6, 1, 5], 8)).toEqual([
+			expect(fn([10, 1, 2, 7, 6, 1, 5], 8).sort()).toEqual([
 				[1, 1, 6],
 				[1, 2, 5],
 				[1, 7],
@@ -12,7 +12,7 @@ fns.forEach((fn) => {
 		});
 
 		it("[2,5,2,1,2]", () => {
-			expect(fn([2, 5, 2, 1, 2], 5)).toEqual([[1, 2, 2], [5]]);
+			expect(fn([2, 5, 2, 1, 2], 5).sort()).toEqual([[1, 2, 2], [5]]);
 		});
 	});
 });
