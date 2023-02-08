7e9cb81271b1321e039b5e4f3f4dc0b63f0313a2
Lanlyhs <ziqiang.chen@outlook.com>
2023-02-08 19:11:21 +0900

    Add 031 use while

--- src/031-next-permutation.js
+++ src/031-next-permutation.js
@@ -21,7 +21,31 @@ module.exports = [
 			if (j === i) {
 				return;
 			}
+			[nums[i], nums[j]] = [nums[j], nums[i]];
+		}
 
+		for (let m = i + 1, n = nums.length - 1; m < n; m++, n--) {
+			[nums[m], nums[n]] = [nums[n], nums[m]];
+		}
+	},
+	/**
+	 * @param {number[]} nums
+	 * @return {void} Do not return anything, modify nums in-place instead.
+	 */
+	function nextPermutation1(nums) {
+		let i = nums.length - 2;
+		while (i >= 0 && nums[i] >= nums[i + 1]) {
+			i--;
+		}
+
+		if (i >= 0) {
+			let j = nums.length - 1;
+			while (j > i && nums[j] <= nums[i]) {
+				j--;
+			}
+			if (j === i) {
+				return;
+			}
 			[nums[i], nums[j]] = [nums[j], nums[i]];
 		}
 
