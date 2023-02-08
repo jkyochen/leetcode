e55e55274da35855f5682dad62abfb218d516331
Lanlyhs <ziqiang.chen@outlook.com>
2023-02-08 18:19:26 +0900

    Add 045 new answer

--- src/045-jump-game-ii.js
+++ src/045-jump-game-ii.js
@@ -17,4 +17,47 @@ module.exports = [
 		}
 		return steps;
 	},
+	/**
+	 * @param {number[]} nums
+	 * @return {number}
+	 */
+	function jump1(nums) {
+		let steps = 0;
+		let maxPosition = 0;
+		let end = 0;
+		for (let i = 0; i < nums.length - 1; i++) {
+			maxPosition = Math.max(maxPosition, i + nums[i]);
+			if (i === end) {
+				end = maxPosition;
+				steps++;
+			}
+		}
+		return steps;
+	},
+	/**
+	 * @param {number[]} nums
+	 * @return {number}
+	 */
+	function jump2(nums) {
+		if (nums.length === 1) {
+			return 0;
+		}
+		let steps = 1;
+		let start = 0;
+		let end = start + nums[start];
+		while (end < nums.length - 1) {
+			steps++;
+			let maxPosition = -1;
+			const s = start;
+			const e = end;
+			for (let i = s; i <= e; i++) {
+				if (nums[i] + i > maxPosition) {
+					maxPosition = nums[i] + i;
+					start = i;
+					end = start + nums[start];
+				}
+			}
+		}
+		return steps;
+	},
 ];
