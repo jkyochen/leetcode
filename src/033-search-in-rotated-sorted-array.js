8a78ce576c1bffe30cc4f18b04785a3a6c728891
Lanlyhs <ziqiang.chen@outlook.com>
2023-02-11 12:03:22 +0900

    Add 033 new answer

--- src/033-search-in-rotated-sorted-array.js
+++ src/033-search-in-rotated-sorted-array.js
@@ -45,4 +45,34 @@ module.exports = [
 		}
 		return -1;
 	},
+	/**
+	 * @param {number[]} nums
+	 * @param {number} target
+	 * @return {number}
+	 */
+	function search2(nums, target) {
+		let l = 0;
+		let r = nums.length - 1;
+		while (l <= r) {
+			let mid = (l + r) >> 1;
+			if (nums[mid] === target) {
+				return mid;
+			}
+
+			if (nums[l] <= nums[mid]) {
+				if (target >= nums[l] && target < nums[mid]) {
+					r = mid - 1;
+				} else {
+					l = mid + 1;
+				}
+			} else {
+				if (target > nums[mid] && target <= nums[r]) {
+					l = mid + 1;
+				} else {
+					r = mid - 1;
+				}
+			}
+		}
+		return -1;
+	},
 ];
