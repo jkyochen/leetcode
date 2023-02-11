3a407b26b6ea7a0bd0b577d924deabec2ebc3376
Lanlyhs <ziqiang.chen@outlook.com>
2023-02-11 10:28:48 +0900

    Update 033 use while

--- src/033-search-in-rotated-sorted-array.js
+++ src/033-search-in-rotated-sorted-array.js
@@ -13,7 +13,9 @@ module.exports = [
 	 * @return {number}
 	 */
 	function search1(nums, target) {
-		for (let i = 0, j = nums.length - 1; i <= j; ) {
+		let i = 0;
+		let j = nums.length - 1;
+		while (i <= j) {
 			let mid = Math.floor((i + j) / 2);
 			if (nums[mid] === target) {
 				return mid;
