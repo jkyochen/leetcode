7767d5d6a4b4cc64b938d5ba952c132e9f99f5b0
Lanlyhs <ziqiang.chen@outlook.com>
2023-02-11 20:20:57 +0900

    Add 029

---
+++ src/029-divide-two-integers.js
@@ -0,0 +1,17 @@
+module.exports = [
+	/**
+	 * @param {number} dividend
+	 * @param {number} divisor
+	 * @return {number}
+	 */
+	function divide(dividend, divisor) {
+		let result = dividend / divisor;
+		if (result >= 0) {
+			result = Math.floor(result);
+			return result <= 2 ** 31 - 1 ? result : 2 ** 31 - 1;
+		} else {
+			result = Math.ceil(result);
+			return result >= -(2 ** 31) ? result : -(2 ** 31);
+		}
+	},
+];
