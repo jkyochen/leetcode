c5cd5d0f162221fb1f480ecf56864c326f70a438
Lanlyhs <ziqiang.chen@outlook.com>
2023-02-16 21:26:32 +0900

    Update change tests key name

--- leetcode/058-length-of-last-word_test.go
+++ leetcode/058-length-of-last-word_test.go
@@ -6,32 +6,32 @@ import (
 
 func TestLengthOfLastWord(t *testing.T) {
 	tests := []struct {
-		s    string
-		want int
+		args    string
+		expected int
 	}{
 		{
-			s:    "Hello World",
-			want: 5,
+			args:    "Hello World",
+			expected: 5,
 		},
 		{
-			s:    "   fly me   to   the moon  ",
-			want: 4,
+			args:    "   fly me   to   the moon  ",
+			expected: 4,
 		},
 		{
-			s:    "luffy is still joyboy",
-			want: 6,
+			args:    "luffy is still joyboy",
+			expected: 6,
 		},
 		{
-			s:    "",
-			want: 0,
+			args:    "",
+			expected: 0,
 		},
 	}
 
 	for _, tc := range tests {
 		for _, v := range lengthOfLastWords {
-			result := v(tc.s)
-			if result != tc.want {
-				t.Fatalf(`Test func:"%s" case:"%v" failed — Expected %v, got %v`, GetFunctionName(v), tc.s, tc.want, result)
+			result := v(tc.args)
+			if result != tc.expected {
+				t.Fatalf(`Test func:"%s" case:"%v" failed — Expected %v, got %v`, GetFunctionName(v), tc.args, tc.expected, result)
 			}
 		}
 	}
--- leetcode/066-plus-one_test.go
+++ leetcode/066-plus-one_test.go
@@ -6,32 +6,32 @@ import (
 
 func TestPlusOne(t *testing.T) {
 	tests := []struct {
-		s    []int
-		want []int
+		args    []int
+		expected []int
 	}{
 		{
-			s:    []int{1, 2, 3},
-			want: []int{1, 2, 4},
+			args:    []int{1, 2, 3},
+			expected: []int{1, 2, 4},
 		},
 		{
-			s:    []int{4, 3, 2, 1},
-			want: []int{4, 3, 2, 2},
+			args:    []int{4, 3, 2, 1},
+			expected: []int{4, 3, 2, 2},
 		},
 		{
-			s:    []int{0},
-			want: []int{1},
+			args:    []int{0},
+			expected: []int{1},
 		},
 		{
-			s:    []int{9, 9},
-			want: []int{1, 0, 0},
+			args:    []int{9, 9},
+			expected: []int{1, 0, 0},
 		},
 	}
 
 	for _, tc := range tests {
 		for _, v := range plusOnes {
-			result := v(append([]int{}, tc.s...))
-			if !Equal(result, tc.want) {
-				t.Fatalf(`Test func:"%s" case:"%v" failed — Expected %v, got %v`, GetFunctionName(v), tc.s, tc.want, result)
+			result := v(append([]int{}, tc.args...))
+			if !Equal(result, tc.expected) {
+				t.Fatalf(`Test func:"%s" case:"%v" failed — Expected %v, got %v`, GetFunctionName(v), tc.args, tc.expected, result)
 			}
 		}
 	}
