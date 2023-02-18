800d65a2db8df6da520142292f8dacdbfc24c732
Lanlyhs <ziqiang.chen@outlook.com>
2023-02-18 23:18:40 +0900

    Update use map return's bool to judge key is exist

--- leetcode/070-climbing-stairs.go
+++ leetcode/070-climbing-stairs.go
@@ -13,14 +13,18 @@ func climbStairs(n int) int {
 	if n == 2 {
 		return 2
 	}
-	res1, res2 := climbCache[n-1], climbCache[n-2]
-	if res1 == 0 {
+
+	res1, b1 := climbCache[n-1]
+	if !b1 {
 		res1 = climbStairs(n - 1)
 		climbCache[n-1] = res1
 	}
-	if res2 == 0 {
+
+	res2, b2 := climbCache[n-2]
+	if !b2 {
 		res2 = climbStairs(n - 2)
 		climbCache[n-2] = res2
 	}
+
 	return res1 + res2
 }
