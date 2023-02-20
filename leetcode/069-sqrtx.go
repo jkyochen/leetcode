e87077d5e2acd0b811d0b0133238efcda0086c06
Lanlyhs <ziqiang.chen@outlook.com>
2023-02-20 19:55:11 +0900

    Update 069 new answer

--- leetcode/069-sqrtx.go
+++ leetcode/069-sqrtx.go
@@ -1,7 +1,11 @@
 package leetcode
 
+import "math"
+
 var mySqrts = []func(a int) int{
 	mySqrt,
+	mySqrt1,
+	mySqrt2,
 }
 
 func mySqrt(x int) int {
@@ -24,5 +28,36 @@ func mySqrt(x int) int {
 			j = mid - 1
 		}
 	}
+	return -1
+}
+
+func mySqrt1(x int) int {
+	if x == 0 {
+		return 0
+	}
+	result := int(math.Exp(0.5 * math.Log(float64(x))))
+	if (result+1)*(result+1) <= x {
+		return result + 1
+	}
+	return result
+}
+
+func mySqrt2(x int) int {
+	result := -1
+	l, r := 0, x
+	for l <= r {
+		mid := (l + r) / 2
+		if mid*mid <= x {
+			l = mid + 1
+			result = mid
+		} else {
+			r = mid - 1
+		}
+	}
+	return result
+}
+
+// TODO Newton's method
+func mySqrt3(x int) int {
 	return -1
 }
