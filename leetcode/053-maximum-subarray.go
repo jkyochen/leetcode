7d86265ef2b29bd01a50b0bcb16e4f534201a8a7
Lanlyhs <ziqiang.chen@outlook.com>
2023-03-04 21:25:52 +0900

    Add 053 new answer

--- leetcode/053-maximum-subarray.go
+++ leetcode/053-maximum-subarray.go
@@ -4,6 +4,7 @@ import "math"
 
 var maxSubArrays = []func(nums []int) int{
 	maxSubArray,
+	maxSubArray1,
 }
 
 func maxSubArray(nums []int) int {
@@ -18,3 +19,18 @@ func maxSubArray(nums []int) int {
 	}
 	return maxValue
 }
+
+func maxSubArray1(nums []int) int {
+	max := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i]+nums[i-1] > nums[i] {
+			nums[i] += nums[i-1]
+		}
+		if nums[i] > max {
+			max = nums[i]
+		}
+	}
+	return max
+}
+
+// TODO segment tree
