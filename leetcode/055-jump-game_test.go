f537f806ce2e36a1d8d6af589c35e0474262e418
Lanlyhs <ziqiang.chen@outlook.com>
2023-03-05 22:39:55 +0900

    Add 055

---
+++ leetcode/055-jump-game_test.go
@@ -0,0 +1,46 @@
+package leetcode
+
+import (
+	"testing"
+)
+
+func TestCanJump(t *testing.T) {
+	tests := []struct {
+		args     []int
+		expected bool
+	}{
+		// {
+		// 	args:     []int{2, 3, 1, 1, 4},
+		// 	expected: true,
+		// },
+		// {
+		// 	args:     []int{3, 2, 1, 0, 4},
+		// 	expected: false,
+		// },
+		// {
+		// 	args:     []int{2, 0, 0},
+		// 	expected: true,
+		// },
+		// {
+		// 	args:     []int{0},
+		// 	expected: true,
+		// },
+		// {
+		// 	args:     []int{2, 0},
+		// 	expected: true,
+		// },
+		{
+			args:     []int{1, 1, 1, 0},
+			expected: true,
+		},
+	}
+
+	for _, tc := range tests {
+		for _, v := range canJumps {
+			result := v(tc.args)
+			if result != tc.expected {
+				t.Fatalf(`Test func:"%s" case:"%v" failed — Expected %v, got %v`, GetFunctionName(v), tc.args, tc.expected, result)
+			}
+		}
+	}
+}
