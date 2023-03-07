0975e410afddca4008c2c405d6e719c0152be340
Lanlyhs <ziqiang.chen@outlook.com>
2023-03-07 16:33:56 +0900

    Update add new

--- leetcode/056-merge-intervals.go
+++ leetcode/056-merge-intervals.go
@@ -4,6 +4,7 @@ import "sort"
 
 var mergeIntervalss = []func(intervals [][]int) [][]int{
 	mergeIntervals,
+	mergeIntervals1,
 }
 
 func mergeIntervals(intervals [][]int) [][]int {
@@ -17,6 +18,22 @@ func mergeIntervals(intervals [][]int) [][]int {
 			mVal[1] = Max(mVal[1], val[len(val)-1])
 		} else {
 			merged = append(merged, val)
+		}
+	}
+	return merged
+}
+
+func mergeIntervals1(intervals [][]int) [][]int {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	merged := [][]int{}
+	for i := 0; i < len(intervals); i++ {
+		L, R := intervals[i][0], intervals[i][1]
+		if len(merged) == 0 || (merged[len(merged)-1][1]+1 != L && merged[len(merged)-1][1] < L) {
+			merged = append(merged, []int{L, R})
+		} else {
+			merged[len(merged)-1][1] = Max(merged[len(merged)-1][1], R)
 		}
 	}
 	return merged
