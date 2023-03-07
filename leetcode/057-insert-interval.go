5f76bd2f91622a58926a93444a50309766cf456f
Lanlyhs <ziqiang.chen@outlook.com>
2023-03-07 20:47:30 +0900

    Update 057 new

--- leetcode/057-insert-interval.go
+++ leetcode/057-insert-interval.go
@@ -4,6 +4,7 @@ import "sort"
 
 var insertIntervals = []func(intervals [][]int, newInterval []int) [][]int{
 	insertInterval,
+	insertInterval1,
 }
 
 func insertInterval(intervals [][]int, newInterval []int) [][]int {
@@ -21,4 +22,21 @@ func insertInterval(intervals [][]int, newInterval []int) [][]int {
 		return inserted[i][0] <= inserted[j][0]
 	})
 	return inserted
+}
+
+func insertInterval1(intervals [][]int, newInterval []int) [][]int {
+	inserted := [][]int{}
+	for i := 0; i < len(intervals); i++ {
+		L, R := intervals[i][0], intervals[i][1]
+		if newInterval[0] > R {
+			inserted = append(inserted, intervals[i])
+			continue
+		}
+		if newInterval[1] < L {
+			inserted = append(inserted, newInterval)
+			return append(inserted, intervals[i:]...)
+		}
+		newInterval = []int{Min(L, newInterval[0]), Max(R, newInterval[1])}
+	}
+	return append(inserted, newInterval)
 }
