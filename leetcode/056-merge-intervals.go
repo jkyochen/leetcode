dec59da74187e478f204a7abe60a8dcba8f6cf6a
Lanlyhs <ziqiang.chen@outlook.com>
2023-03-07 16:03:25 +0900

    Update change variable name to merged

--- leetcode/056-merge-intervals.go
+++ leetcode/056-merge-intervals.go
@@ -10,16 +10,16 @@ func mergeIntervals(intervals [][]int) [][]int {
 	sort.Slice(intervals, func(i, j int) bool {
 		return intervals[i][0] < intervals[j][0]
 	})
-	mergedIntervals := [][]int{intervals[0]}
+	merged := [][]int{intervals[0]}
 	for i := 1; i < len(intervals); i++ {
 		val := intervals[i]
-		mInterval := mergedIntervals[len(mergedIntervals)-1]
+		mInterval := merged[len(merged)-1]
 		mEnd := mInterval[len(mInterval)-1]
 		if val[0] <= mEnd || val[0]+1 == mEnd {
 			mInterval[len(mInterval)-1] = Max(mInterval[len(mInterval)-1], val[len(val)-1])
 		} else {
-			mergedIntervals = append(mergedIntervals, val)
+			merged = append(merged, val)
 		}
 	}
-	return mergedIntervals
+	return merged
 }
