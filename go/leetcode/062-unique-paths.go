1e1167caf3bf09d083de0335601feccc4731fd88
ChenZiQiang <ziqiang.chen@outlook.com>
2023-03-14 19:56:26 +0900

    Update 062 remove variable uniquePathCount

--- go/leetcode/062-unique-paths.go
+++ go/leetcode/062-unique-paths.go
@@ -13,22 +13,21 @@ var uniquePathss = []func(m int, n int) int{
 }
 
 func uniquePaths(m int, n int) int {
-	uniquePathCount := 0
-	var recursion func(x int, y int)
-	recursion = func(x int, y int) {
+	var recursion func(x int, y int) int
+	recursion = func(x int, y int) int {
 		if x == m && y == n {
-			uniquePathCount++
-			return
+			return 1
 		}
+		xRecurCount, yRecurCount := 0, 0
 		if x < m {
-			recursion(x+1, y)
+			xRecurCount = recursion(x+1, y)
 		}
 		if y < n {
-			recursion(x, y+1)
+			yRecurCount = recursion(x, y+1)
 		}
+		return xRecurCount + yRecurCount
 	}
-	recursion(1, 1)
-	return uniquePathCount
+	return recursion(1, 1)
 }
 
 func uniquePaths1(m int, n int) int {
