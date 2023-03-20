3cbd7cc59f0919152a7a6db76e84a216c85a7794
ChenZiQiang <ziqiang.chen@outlook.com>
2023-03-20 22:38:56 +0900

    Add 064 dp

--- go/leetcode/064-minimum-path-sum.go
+++ go/leetcode/064-minimum-path-sum.go
@@ -4,6 +4,7 @@ import "math"
 
 var minPathSums = []func(grid [][]int) int{
 	minPathSum,
+	minPathSum1,
 }
 
 func minPathSum(grid [][]int) int {
@@ -24,4 +25,28 @@ func minPathSum(grid [][]int) int {
 	}
 	recursion(0, 0, 0)
 	return minSum
+}
+
+func minPathSum1(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
+	dpTable := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dpTable[i] = make([]int, n)
+		if i == 0 {
+			dpTable[i][0] = grid[i][0]
+			continue
+		}
+		dpTable[i][0] = dpTable[i-1][0] + grid[i][0]
+	}
+	for j := 1; j < n; j++ {
+		dpTable[0][j] = dpTable[0][j-1] + grid[0][j]
+	}
+
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dpTable[i][j] = grid[i][j] + Min(dpTable[i-1][j], dpTable[i][j-1])
+		}
+	}
+
+	return dpTable[m-1][n-1]
 }
