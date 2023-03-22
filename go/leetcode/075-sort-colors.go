6badbd312ed8a9994f32cb9463bce85e66dd48ce
ChenZiQiang <ziqiang.chen@outlook.com>
2023-03-22 11:41:23 +0900

    Refactoring remove temporary variable

--- go/leetcode/075-sort-colors.go
+++ go/leetcode/075-sort-colors.go
@@ -5,17 +5,17 @@ var sortColorss = []func(nums []int){
 }
 
 func sortColors(nums []int) {
-	redLow := 0
-	for fast := 0; fast < len(nums); fast++ {
-		if nums[fast] == 0 {
-			nums[fast], nums[redLow] = nums[redLow], nums[fast]
-			redLow++
+	ptr := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == 0 {
+			nums[i], nums[ptr] = nums[ptr], nums[i]
+			ptr++
 		}
 	}
-	for low, fast := redLow, redLow; fast < len(nums); fast++ {
-		if nums[fast] == 1 {
-			nums[fast], nums[low] = nums[low], nums[fast]
-			low++
+	for i := ptr; i < len(nums); i++ {
+		if nums[i] == 1 {
+			nums[i], nums[ptr] = nums[ptr], nums[i]
+			ptr++
 		}
 	}
 }
