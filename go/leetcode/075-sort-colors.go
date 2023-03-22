badeca9a7782599841f7f6bafe91586e8e8ab373
ChenZiQiang <ziqiang.chen@outlook.com>
2023-03-22 11:55:54 +0900

    Add 075 new method use less code

--- go/leetcode/075-sort-colors.go
+++ go/leetcode/075-sort-colors.go
@@ -2,6 +2,7 @@ package leetcode
 
 var sortColorss = []func(nums []int){
 	sortColors,
+	sortColors1,
 }
 
 func sortColors(nums []int) {
@@ -18,4 +19,18 @@ func sortColors(nums []int) {
 			ptr++
 		}
 	}
+}
+
+func sortColors1(nums []int) {
+	var innerSortColor = func(colors []int, target int) (ptr int) {
+		for i, v := range colors {
+			if v == target {
+				colors[i], colors[ptr] = colors[ptr], colors[i]
+				ptr++
+			}
+		}
+		return
+	}
+	ptr := innerSortColor(nums, 0)
+	innerSortColor(nums[ptr:], 1)
 }
