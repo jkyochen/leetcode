92f33a552cb6c9f3e73441035d1f41d185d7b4a1
ChenZiQiang <ziqiang.chen@outlook.com>
2023-03-24 18:42:40 +0900

    Add 077 new answer

--- go/leetcode/077-combinations.go
+++ go/leetcode/077-combinations.go
@@ -2,6 +2,7 @@ package leetcode
 
 var combines = []func(n int, k int) [][]int{
 	combine,
+	combine1,
 }
 
 func combine(n int, k int) [][]int {
@@ -21,3 +22,30 @@ func combine(n int, k int) [][]int {
 	innerCombine([]int{}, 1)
 	return result
 }
+
+func combine1(n int, k int) [][]int {
+	ans := [][]int{}
+	temp := []int{}
+	var dfs func(cur int)
+	dfs = func(cur int) {
+		if len(temp)+(n-cur+1) < k {
+			return
+		}
+		if len(temp) == k {
+			comb := make([]int, k)
+			copy(comb, temp)
+			ans = append(ans, comb)
+			return
+		}
+		// pick curent
+		temp = append(temp, cur)
+		dfs(cur + 1)
+		temp = temp[:len(temp)-1]
+		// no pick current, pick next
+		dfs(cur + 1)
+	}
+	dfs(1)
+	return ans
+}
+
+// TOD Lexicographic order method
