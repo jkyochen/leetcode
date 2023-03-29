48fd099936a33c5cfe854481e234b42f32a2f011
ChenZiQiang <ziqiang.chen@outlook.com>
2023-03-29 18:14:37 +0900

    Add 082 new answer

--- go/leetcode/082-remove-duplicates-from-sorted-list-ii.go
+++ go/leetcode/082-remove-duplicates-from-sorted-list-ii.go
@@ -2,6 +2,7 @@ package leetcode
 
 var deleteDuplicates2s = []func(head *ListNode) *ListNode{
 	deleteDuplicates2,
+	deleteDuplicates2Two,
 }
 
 func deleteDuplicates2(head *ListNode) *ListNode {
@@ -31,4 +32,25 @@ func deleteDuplicates2(head *ListNode) *ListNode {
 		curNode = curNode.Next
 	}
 	return preNode.Next
+}
+
+func deleteDuplicates2Two(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+
+	dummy := &ListNode{0, head}
+	cur := dummy
+	for cur.Next != nil && cur.Next.Next != nil {
+		if cur.Next.Val == cur.Next.Next.Val {
+			v := cur.Next.Val
+			for cur.Next != nil && cur.Next.Val == v {
+				cur.Next = cur.Next.Next
+			}
+		} else {
+			cur = cur.Next
+		}
+	}
+
+	return dummy.Next
 }
